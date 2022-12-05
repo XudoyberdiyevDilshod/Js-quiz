2e006fd8a502e5487e20b41afe7fea31adb6f112
XudoyberdiyevDilshod <1998xudoyberdiev@gmail.com>
2022-12-06 01:27:59 +0500

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,44 +1,48 @@
-// 1 - assignment
+// ----------------- 1 - assignment -------------
 function addition(a, b) {
 	return a + b;
 }
 
-// 2 - assignment
+//----------------- 2 - assignment ---------------
 function convert(minutes) {
 	return minutes * 60;
 }
 
-// 3 - assignment
+//----------------- 3 - assignment------------------
 function addition(num) {
 	return num + 1;
 }
 
-// 5 - assignment
+//----------------- 4 - assignment------------------
 function triArea(base, height) {
 	return (base * height) / 2;
 }
+//----------------- 5 - assignment------------------
+function calcAge(age) {
+	return age * 365;
+}
 
-// 6 - assignment
+//----------------- 6 - assignment------------------
 function remainder(x, y) {
 	return x %= y;
 }
 
-// 7 - assignment
+//----------------- 7 - assignment------------------
 function footballPoints(wins, draws, losses) {
 	return wins * 3 + draws * 1 + losses * 0;
 }
 
-// 8 - assignment
+//----------------- 8 - assignment------------------
 function points(twoPointers, threePointers) {
 	return twoPointers * 2 + threePointers * 3;
 }
 
-// 9 - assignment
+//----------------- 9 - assignment------------------
 function lessThan100(a, b) {
 	return (a + b < 100) ? true : false;
 }
 
-// 10 - assignment
+//----------------- 10 - assignment------------------
 function animals(chickens, cows, pigs) {
 	return chickens * 2 + cows * 4 + pigs * 4 ;
 }
